0f2a98ce82985c7f6452b144d16fbac6f86706d9
Adittoguy <adityasanap2001@gmail.com>
2025-11-03 23:42:40 +0530

    GUI Codes

---
+++ EvenHandling1.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+class EvenHandling1
+{
+    public static void main(String A[])
+    {
+        JFrame fobj = new JFrame("PPA");
+
+        JButton bobj = new JButton("OK");
+        
+        bobj.setBounds(100, 100, 150, 50); // (X , Y, width, height)
+
+        bobj.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent aobj)
+            {
+                // logic inside this will execute when button is clicked
+                System.out.println("Button Clicked....");
+            }
+
+        });
+
+        fobj.add(bobj);
+
+        fobj.setSize(400, 300);
+
+        fobj.setLayout(null);
+        fobj.setVisible(true);
+
+        fobj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
